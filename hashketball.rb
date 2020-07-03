1d9bbe2c8732ba7bdf7e800948249e23017ebdbc
Ward Price <wardprice@gmail.com>
2020-07-03 17:03:08 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,27 @@
 # Write your code here!
+def game_hash
+  {
+    home: {
+      team_name: "Brooklyn Nets",
+      colors: ["Black", "White"]
+      players:[{
+        player_name:
+        number:
+        shoe:
+        points:
+        rebounds:
+        assists:
+        steals:
+        blocks:
+        slam_dunks:
+      }]
+    }
+    away: {
+      team_name:
+      colors:
+      players: [{
+        
+      }]
+    }
+  }
+end 
